ccf47ce845bbce5a54b5da7ef46a86f378d1a1cc
Danila Kulinkovich <81530683+kulick23@users.noreply.github.com>
2023-12-15 20:54:43 +0200

    Add files via upload

---
+++ UPDATE_Danila_Kulinkovich.sql
@@ -0,0 +1,36 @@
+-- 1 
+UPDATE film
+SET rental_duration = 21, rental_rate = 9.99
+WHERE title = 'The Green Mile';
+
+
+-- 2
+UPDATE customer
+SET first_name = 'YourFirstName', last_name = 'YourLastName', address_id = (
+    SELECT address_id
+    FROM address
+    ORDER BY RANDOM()
+    LIMIT 1
+)
+WHERE customer_id = (
+    SELECT customer.customer_id
+    FROM customer
+    JOIN rental ON customer.customer_id = rental.customer_id
+    JOIN payment ON customer.customer_id = payment.customer_id
+    GROUP BY customer.customer_id
+    HAVING COUNT(DISTINCT rental.rental_id) >= 10 AND COUNT(DISTINCT payment.payment_id) >= 10
+    LIMIT 1
+);
+
+-- 3
+UPDATE customer
+SET create_date = CURRENT_DATE
+WHERE customer_id = (
+    SELECT customer_id
+    FROM customer
+    ORDER BY RANDOM()
+    LIMIT 1
+)
+RETURNING *;
+
+
